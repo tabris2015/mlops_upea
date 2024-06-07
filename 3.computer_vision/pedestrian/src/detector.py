51996832e5bdea782963d33ce0b43bcf7820dc86
Jose Laruta <eduardo.laruta@gmail.com>
2024-06-07 15:12:17 -0400

    4ta clase

--- 3.computer_vision/pedestrian/src/detector.py
+++ 3.computer_vision/pedestrian/src/detector.py
@@ -26,8 +26,18 @@ class PedestrianDetector:
     def predict_image(self, image_array, threshold):
         results = self.model(image_array, conf=threshold)[0]
         labels = results.boxes.cls.tolist()
-        indexes = [i for i in range(len(labels)) if labels[i] == 0]     # 0 = "person"
-        boxes = [[int(v) for v in box] for i, box in enumerate(results.boxes.xyxy.tolist()) if i in indexes]
+        #   0 = person
+        # [0, 2, 0, 3, 0]
+        #  0  1  2  3  4
+        # [0, 2, 4]
+        #
+
+        indexes = [i for i in range(len(labels)) if labels[i] in [0, 15, 16]]     # 0 = "person"
+        boxes = [
+            [int(v) for v in box] 
+            for i, box in enumerate(results.boxes.xyxy.tolist()) 
+            if i in indexes
+        ]
         confidences = [c for i, c in enumerate(results.boxes.conf.tolist()) if i in indexes]
         detection = Detection(
             pred_type=PredictionType.object_detection,
